b4934d2f4cffded6f30aa179e2371ae88b1b5db0
Tristan Tan Jing Xuan <jtan0375@student.monash.edu>
2025-04-23 21:06:06 +0800

    update

---
+++ kwong.py
@@ -0,0 +1,18 @@
+def get_amp() :
+    # input  : a list of raw data from stm  
+    # output : the highest amplitude from the list 
+    
+    # anything you find relavant to our project 
+    # only take highest amplitude ?
+    # take multiple highest ampltiude if there's different spikes ? 
+    raise NotImplementedError
+
+
+def visualize_data() : 
+    # input : a list of raw data from stm  
+    # output : plot the graph to visualize the input 
+    raise NotImplementedError
+
+
+
+# plot the raw data 
---
+++ tristan.py
@@ -0,0 +1,13 @@
+def gather_data() : 
+    # input : input from user to get data 
+    # output : start stm 
+    raise NotImplementedError
+
+def get_freq() :
+    # input  : a list of raw data from stm  
+    # output : the frequency from the list 
+    raise NotImplementedError
+
+
+
+# plot the raw data 
